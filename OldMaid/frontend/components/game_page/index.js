25bf8de63b137164791c05e5d66e43cdc7b651c7
koreanpro22 <dhskim22@gmail.com>
2023-12-19 16:32:40 -0500

    add test game page

---
+++ OldMaid/frontend/components/game_page/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native"
+
+export default function GamePage() {
+
+    return (
+        <View>
+            <Text>Test Game Page</Text>
+        </View>
+    )
+
+}
