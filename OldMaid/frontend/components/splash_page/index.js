c5a1a12afd6e2fc7328f78623a39a58a66167f78
Bao <bao4ltyfe@gmail.com>
2023-12-19 13:31:44 -0800

    basic splash page

--- OldMaid/frontend/components/splash_page/index.js
+++ OldMaid/frontend/components/splash_page/index.js
@@ -1,28 +1,30 @@
-import { StyleSheet, Text, View,Button } from 'react-native';
+import React from 'react';
+import { View, Image, Button, StyleSheet } from 'react-native';
 
 const SplashPage = () => {
+    const handleButtonPress = () => {
+        console.log('Button pressed!');
+    };
 
     return (
-        <>
-            <View style={styles.container}>
-                <Text>
-                    Imagehere
-                </Text>
-            </View>
-            <Button
-            title='Start'
-            />
-        </>
-    )
-}
+        <View style={styles.container}>
+            <Image style={styles.image} source={require('../../assets/old-maid.jpg')} />
+            <Button title='Game-ON' color="green" onPress={handleButtonPress} />
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
-  });
+    image: {
+        width: 200,
+        height: 200,
+        resizeMode: 'contain',
+    },
+});
 
-export default SplashPage
+export default SplashPage;
