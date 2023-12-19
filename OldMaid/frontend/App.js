e8295502f404b568e33db403412529f4d679b9f2
koreanpro22 <dhskim22@gmail.com>
2023-12-19 16:22:28 -0500

    add test game page

--- OldMaid/frontend/App.js
+++ OldMaid/frontend/App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import GamePage from components;
 
 export default function App() {
   return (
---
+++ OldMaid/frontend/components/game_page/index.js
@@ -0,0 +1,13 @@
+import { View } from "react-native";
+
+
+export default function GamePage() {
+
+
+    return (
+       <View>
+            <Text>Testing Game Page</Text>
+       </View>
+    )
+
+}
